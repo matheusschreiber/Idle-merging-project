30951dc2ea13af52d8f8c2d92fb4d60d0a4b789a
Matheus Schreiber <mt.schreiber01@gmail.com>
2022-04-24 00:08:42 -0300

    :label: Adding types export

---
+++ types/Aircraft.types.ts
@@ -0,0 +1,7 @@
+export type Aircraft = {
+  id: Number,
+  player_id: Number,
+  level: Number, 
+  money_per_second: Number,
+  bonus_multiplier: Number,
+}
---
+++ types/Player.types.ts
@@ -0,0 +1,8 @@
+export type Player = {
+  id: Number,
+  name: String,
+  password: String,
+  rank: Number,
+  aircrafts: String,
+  wallet: Number
+}
