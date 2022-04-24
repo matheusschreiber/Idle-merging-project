a260b215f3946bd0ce83cd8ddb8dde8479414909
Matheus Schreiber <mt.schreiber01@gmail.com>
2022-04-24 00:11:24 -0300

    :card_file_box: Creating player table migration, and refactoring aircraft table migration

--- database/migrations/20220421143918_Create_aircrafts.js
+++ database/migrations/20220421143918_Create_aircrafts.js
@@ -5,10 +5,11 @@
 exports.up = function(knex) {
   return knex.schema.createTable('aircrafts',(table)=>{
     table.increments().primary().notNullable()
+    table.integer('player_id').unsigned()
+    table.foreign('player_id').references('players.id')
     table.integer('level')
     table.float('money_per_second')
     table.float('bonus_multiplier')
-    table.string('owner')
   })
 };
 
---
+++ database/migrations/20220424011826_Create_players.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+  return knex.schema.createTable('players',(table)=>{
+    table.increments().primary().notNullable()
+    table.string('name')
+    table.string('password')
+    table.integer('rank')
+    table.string('aircrafts')
+    table.float('wallet')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('players')
+};
