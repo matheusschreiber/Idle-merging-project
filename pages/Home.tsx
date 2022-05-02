d7dfa141d4de6c3c0776618104b896fe0313ec37
Matheus Schreiber <mt.schreiber01@gmail.com>
2022-05-01 23:13:19 -0300

    :wrench: update renaming

---
+++ pages/Home.tsx
@@ -0,0 +1,99 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
+import styles from '../styles/Home.module.css'
+import { FiArrowLeft } from 'react-icons/fi'
+
+import { Aircraft } from '../types/Aircraft.types'
+import { Player } from '../types/Player.types'
+
+import { AircraftPanel } from '../components/AircraftPanel'
+import { Header } from '../components/Header'
+import { Ring } from '../components/Ring'
+
+import { handleGetPlayer } from '../services/player'
+
+const Home: NextPage = () => {
+
+  const [ player, setPlayer ] = useState<Player>()
+  const [ showing, setShowing ] = useState<number>(0)
+  const [ flow, setFlow ] = useState<number>(0)
+  
+  async function loadPlayerData(){
+    const player:Player = await handleGetPlayer(1)
+    setPlayer(player)  
+  }     
+
+  function getFlow(f:number){ 
+    setFlow(f)
+   }
+
+  useEffect(()=>{
+    loadPlayerData()
+  }, [])  
+   
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>IDLE</title>
+        <meta name="description" content="Idle game" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <aside>
+        <ul>
+          <li
+            onClick={()=>{
+              let item = document.getElementsByTagName('li')[0]
+              if (!showing) {
+                item.innerHTML = 'SOON'
+                item.style.backgroundColor = 'var(--dark_gray)'
+                item.style.color = 'var(--redish)'
+              } else {
+                item.innerHTML = 'STORE'
+                item.style.backgroundColor = 'var(--gray)'
+                item.style.color = 'var(--dark_gray)'
+              }
+            }}
+            
+            onMouseOver={()=>{
+              let item = document.getElementsByTagName('li')[0]
+              item.style.backgroundColor = 'var(--redish)'
+              item.style.color = 'white'}}
+
+            onMouseOut={()=>{
+              let item = document.getElementsByTagName('li')[0]
+              item.innerHTML = 'STORE'
+              item.style.backgroundColor = 'var(--gray)'
+              item.style.color = 'var(--dark_gray)'}}
+            >
+              STORE
+            </li>
+
+          <li onClick={()=>setShowing(2)}>FLYING</li>
+        </ul>      
+      </aside>
+
+      <main className={styles.main}>
+        {
+          player?
+          <Header playerImported={player} flowImported={flow}/>
+          :"Loading..."
+        }
+
+        <div style={player&&showing==2?{}:{visibility:'hidden'}}>
+          {     player  ? <AircraftPanel player={player} setShowing={setShowing} getFlow={getFlow}/> : ""   }
+        </div>
+
+        { player  ? <Ring player={player}/> : "" }
+
+
+      </main>
+
+      
+    </div>
+  )
+}
+
+export default Home
