29b5125a618712584c659e671c0f8fc524c7863f
Matheus Schreiber <mt.schreiber01@gmail.com>
2022-05-08 19:45:29 -0300

    :pencil2: fix api link

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 const api:AxiosInstance = axios.create({
   // baseURL:`http://localhost:${process.env.NEXT_PUBLIC_PORT}/api`
-  baseURL:`http://idlegame.vercel.app:${process.env.NEXT_PUBLIC_PORT}/api`
+  baseURL:`/api`
   
 })
 
