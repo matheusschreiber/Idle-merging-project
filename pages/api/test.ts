ff38c33da0fbe981103d9fa76b766d8ccc583c9e
Matheus <mt.schreiber01@gmail.com>
2022-08-23 17:37:54 -0300

    :bug: debugging error on vercel

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,5 +1,7 @@
 import { NextApiResponse,NextApiRequest } from "next";
+import connection from '../../database/connection'
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse){
-  res.status(200).json({message:"lol"})
+  const t = await connection('players').select('*')
+  res.status(200).json({message:"lol", t})
 }
